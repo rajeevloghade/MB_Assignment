1b5331123e9196e122230597e939edea6a0a21e9
rajeevloghade <rajeevloghade010@gmail.com>
2021-11-14 17:14:22 +0530

    manager and employee repository created

---
+++ MB_Assignment_Manager_Employee/src/main/java/com/assignment/dao/IEmployeeDao.java
@@ -0,0 +1,12 @@
+package com.assignment.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.assignment.model.Employee;
+
+@Repository("IEmployeeDao")
+public interface IEmployeeDao extends JpaRepository<Employee, Integer> {
+
+	Employee getEmployeeByEmployeeId(Integer employeeId);
+}
---
+++ MB_Assignment_Manager_Employee/src/main/java/com/assignment/dao/IManagerDao.java
@@ -0,0 +1,12 @@
+package com.assignment.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.assignment.model.Manager;
+
+@Repository("IManagerDao")
+public interface IManagerDao extends JpaRepository<Manager, Integer> {
+
+	Manager getManagerByManagerId(Integer managerId);
+}
