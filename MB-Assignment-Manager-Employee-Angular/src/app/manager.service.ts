d0400675d0855b205e1271bf9f873d3a22d0a35d
rajeevloghade <rajeevloghade010@gmail.com>
2021-11-16 17:05:12 +0530

    manager service created & login api implemented

---
+++ MB-Assignment-Manager-Employee-Angular/src/app/manager.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ManagerService {
+  private url = 'http://localhost:8085/mgr';
+
+  constructor(private _httpService: HttpClient) {}
+
+  managerLoginVerification(email: string, password: string):Observable<Object> {
+    const params = new HttpParams()
+      .set('email', email)
+      .set('password', password);
+    return this._httpService.get(this.url+'/managerLoginVerification', { params });
+  }
+}
