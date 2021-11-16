cfc73f99373f7cf5bf7b89abe27f00e71b6970d8
rajeevloghade <rajeevloghade010@gmail.com>
2021-11-16 17:02:59 +0530

    manager & employee ts created

---
+++ MB-Assignment-Manager-Employee-Angular/src/employee.ts
@@ -0,0 +1,13 @@
+export class Employee {
+  'firstName': string;
+  'lastName': string;
+  'email': string;
+  'password': string;
+  'mobile': string;
+  'state': string;
+  'city': string;
+  'street': string;
+  'pinCode': string;
+  'dob': string;
+  'company': string;
+}
