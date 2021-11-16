ca709c4aa72ec12fe77fc948381fa753b464409b
rajeevloghade <rajeevloghade010@gmail.com>
2021-11-16 17:05:56 +0530

    routing applied

--- MB-Assignment-Manager-Employee-Angular/src/app/app-routing.module.ts
+++ MB-Assignment-Manager-Employee-Angular/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManagerComponent } from './manager/manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'managerLogin', pathMatch: 'full' },
+  { path: 'managerLogin', component: ManagerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents=[ManagerComponent]
