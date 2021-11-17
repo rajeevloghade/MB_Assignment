8feaab709cdd5c3f5a3e52b03c947a77e884fb37
rajeevloghade <rajeevloghade010@gmail.com>
2021-11-17 17:50:26 +0530

    employee create component created

--- MB-Assignment-Manager-Employee-Angular/src/app/app-routing.module.ts
+++ MB-Assignment-Manager-Employee-Angular/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { ManagerComponent } from './manager/manager.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'managerLogin', pathMatch: 'full' },
   { path: 'managerLogin', component: ManagerComponent },
   { path: 'employeeList', component: EmployeeListComponent },
-  { path: 'employeeDetails/:employeeId', component: EmployeeDetailsComponent }
+  { path: 'employeeDetails/:employeeId', component: EmployeeDetailsComponent },
+  { path: 'addEmployee', component: EmployeeCreateComponent }
 ];
 
 @NgModule({
@@ -20,4 +22,5 @@ export const routingComponents = [
   ManagerComponent,
   EmployeeListComponent,
   EmployeeDetailsComponent,
+  EmployeeCreateComponent
 ];
