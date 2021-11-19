8004e0c759992b44b8db899a59fe65a812581dfc
rajeevloghade <rajeevloghade010@gmail.com>
2021-11-19 14:40:09 +0530

    Revert "add employee form & api implemented"
    This reverts commit 3964c04810333aa58b0ae24126501f9e56a4fc0a.

--- MB-Assignment-Manager-Employee-Angular/src/app/employee-create/employee-create.component.ts
+++ MB-Assignment-Manager-Employee-Angular/src/app/employee-create/employee-create.component.ts
@@ -1,43 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Employee } from 'src/employee';
-import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-create',
   templateUrl: './employee-create.component.html',
-  styleUrls: ['./employee-create.component.css'],
+  styleUrls: ['./employee-create.component.css']
 })
 export class EmployeeCreateComponent implements OnInit {
-  public employee = new Employee();
-  constructor(
-    private _employeeService: EmployeeService,
-    private route: Router
-  ) {}
 
-  ngOnInit(): void {}
+  constructor() { }
 
-  onSubmit() {
-    this.addEmployee();
+  ngOnInit(): void {
   }
 
-  addEmployee() {
-    this._employeeService
-      .addEmployee(this.employee)
-      .subscribe((response: any) => {
-        console.log(response);
-        if(response!=null){
-          this.gotoEmployeeList();
-        }else{
-          this.goToAddEmployee();
-        }
-      });
-  }
-  gotoEmployeeList() {
-    this.route.navigate(['employeeList']);
-  }
-
-  goToAddEmployee(){
-    this.route.navigate(['addEmployee']);
-  }
 }
--- MB-Assignment-Manager-Employee-Angular/src/app/employee.service.ts
+++ MB-Assignment-Manager-Employee-Angular/src/app/employee.service.ts
@@ -24,11 +24,4 @@ export class EmployeeService {
       this.url + 'getEmployeeById/' + employeeId
     );
   }
-
-  addEmployee(employee: Employee) {
-    let header = { 'content-type': 'application/json' };
-    let options = { headers: header };
-    console.log(employee);
-    return this._httpService.post(this.url + 'addEmployee', employee, options);
-  }
 }
