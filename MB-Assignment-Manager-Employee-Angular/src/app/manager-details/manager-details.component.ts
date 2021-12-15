9a3600018b5edbfa62940ca21468b989c49cb479
rajeevloghade <rajeevloghade010@gmail.com>
2021-12-15 21:42:16 +0530

    Manager by id api

--- MB-Assignment-Manager-Employee-Angular/src/app/manager-details/manager-details.component.ts
+++ MB-Assignment-Manager-Employee-Angular/src/app/manager-details/manager-details.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Manager } from 'src/manager';
+import { ManagerService } from '../manager.service';
 
 @Component({
   selector: 'app-manager-details',
   templateUrl: './manager-details.component.html',
-  styleUrls: ['./manager-details.component.css']
+  styleUrls: ['./manager-details.component.css'],
 })
 export class ManagerDetailsComponent implements OnInit {
+  public manager = new Manager();
+  public managerId: number | undefined;
 
-  constructor() { }
+  constructor(
+    private managerService: ManagerService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.managerId = parseInt(this.route.snapshot.paramMap.get('managerId') as string);
+    console.log("managerId : ",this.managerId);
+    this.getManagerById(this.managerId);
   }
 
+  getManagerById(managerId: number) {
+    this.managerService.getManagerById(managerId).subscribe((managerById) => {
+      this.manager = managerById;
+      console.log(this.manager);
+    });
+  }
 }
--- MB-Assignment-Manager-Employee-Angular/src/app/manager.service.ts
+++ MB-Assignment-Manager-Employee-Angular/src/app/manager.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpClient,
-  HttpParams,
-} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Manager } from 'src/manager';
 import { Response } from 'src/Response';
@@ -22,9 +19,12 @@ export class ManagerService {
     const params = new HttpParams()
       .set('email', email)
       .set('password', password);
-    return this._httpService.get<Response>(this.url + 'managerLoginVerification', {
-      params,
-    });
+    return this._httpService.get<Response>(
+      this.url + 'managerLoginVerification',
+      {
+        params,
+      }
+    );
   }
 
   managerSignup(manager: Manager) {
@@ -32,5 +32,10 @@ export class ManagerService {
     let options = { headers: header };
     console.log(manager);
     return this._httpService.post(this.url + 'managerSignup', manager, options);
+  }
+  getManagerById(managerId: number) {
+    return this._httpService.get<Manager>(
+      this.url + 'getManagerByEmployeeId/' + managerId
+    );
   }
 }
